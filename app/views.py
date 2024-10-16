95c5d6be43ee6e1e5feb41671ff4a35dafc11172
patrick <pgonzalez25@basecampcodingacademy.org>
2024-10-16 11:15:52 -0500

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+# Create your views here.
+def hello_view(request: HttpRequest)-> HttpResponse:
+    return HttpResponse('Hello World!')
